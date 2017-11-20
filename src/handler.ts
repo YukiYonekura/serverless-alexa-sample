0312864719b33e4092a77605986851d9a950a305
YukiYonekura <yuki.yonekura@myu-on.co.jp>
2017-11-20 18:58:35 +0900

    first commit

---
+++ src/handler.ts
@@ -0,0 +1,17 @@
+import * as Alexa from 'alexa-sdk'
+
+module.exports.hello = (event: Alexa.RequestBody<any>, context: Alexa.Context) => {
+  const alexa = Alexa.handler(event, context)
+  alexa.registerHandlers({
+    'Unhandled': function () {
+      this.emit(':tell', 'よく分かりません')
+    },
+    'LaunchRequest': function () {
+      this.emit(':tell', 'こんにちはアレクサ')
+    },
+    'AMAZON.StopIntent': function () {
+      this.emit(':tell', 'さようならアレクサ')
+    }
+  })
+  alexa.execute()
+}
