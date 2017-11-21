335f75c25caaace3a436143fa6c4292de3e1bd45
YukiYonekura <yuki.yonekura@myu-on.co.jp>
2017-11-21 14:24:35 +0900

    アプリケーションIDを設定

--- src/handler.ts
+++ src/handler.ts
@@ -2,6 +2,7 @@ import * as Alexa from 'alexa-sdk'
 
 module.exports.hello = (event: Alexa.RequestBody<any>, context: Alexa.Context) => {
   const alexa = Alexa.handler(event, context)
+  alexa.appId = process.env.APPLICATION_ID
   alexa.registerHandlers({
     'Unhandled': function () {
       this.emit(':tell', 'よく分かりません')
